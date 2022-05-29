a39d1f1cd34ed90c79fdff79cf86ab0e6a09559f
ahmad2743 <amethdione10@gmail.com>
2022-05-29 15:39:00 +0200

    Initial Commit

---
+++ SocialNetworkApp/SocialNetworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkAppApp.swift
+//  SocialNetworkApp
+//
+//  Created by Ahamad Ben on 29/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
